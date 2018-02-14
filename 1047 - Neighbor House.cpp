1a00a12646c320812a9d1f46f1d12146fa41f502
raihan02 <raihanraju02@gmail.com>
2018-02-14 20:18:38 +0600

    Update 1047 - Neighbor House.cpp

--- 1047 - Neighbor House.cpp
+++ 1047 - Neighbor House.cpp
@@ -1,58 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
-int dp[21][1003];
-int arr[100][100];
+int arr[1001][1001];
 int n;
-int min_cost (int i , int j)
+int p[101][101];
+int dp (int i, int j)
 {
-    int a, b, c;
     if(i == n)
+    {
         return arr[i][j];
-    if(dp[i][j] != -1)
-        return dp[i][j];
-    if(j == 1)
-    {
-       a = 2;
-       b = 3;
     }
-    if(j == 2)
-    {
-        a = 1;
-        b = 3;
-    }
-    if(j == 3)
-    {
-        a = 1;
-        b = 2;
-    }
-    int x = arr[i][j] + min_cost(i + 1 , a);
-    int y = arr[i][j] + min_cost(i + 1 , b);
-    //printf("%d %d\n",x,y);
-    dp[i][j] = min(x, y);
+    int ret = 999999999;
+    if(p[i][j] != -1){
 
-    return dp[i][j];
+        return p[i][j];
+
+    }
+
+    for(int k = 1; k <= 3; k++)
+    {
+        if(j == k)
+            continue;
+       ret = min(ret,dp(i+ 1, k));
+
+    }
+     ret += arr[i][j];
+     p[i][j] = ret;
+
+    return p[i][j];
 }
 int main()
 {
-     int tes;
-     cin >> tes;
-     int o = 0;
+    int tes,o=0;
+    scanf("%d", &tes);
+
     while(tes--)
     {
+        memset(p, -1, sizeof p);
+
+        scanf("%d", &n);
         o++;
-        memset(dp , -1, sizeof dp);
-        cin >> n;
         for(int i = 1; i <= n; i++)
         {
             for(int j = 1; j <= 3; j++)
             {
-
-                cin >> arr[i][j];
+                scanf("%d", &arr[i][j]);
             }
         }
-        int x = min(min_cost(1 , 1) , min(min_cost(1, 2) , min_cost(1, 3)));
 
-        printf("Case %d: %d\n",o,x);
+        int x = dp(0,0);
+
+        printf("Case %d: %d\n",o, x);
     }
 }
